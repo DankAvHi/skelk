78229fded916a72c5c60ed0a033d2a98f5529bc2
DankAvHi <thexcat86@gmail.com>
2022-12-12 12:23:49 +0300

    small

--- client/src/Pages/Home/components/Header/Header.tsx
+++ client/src/Pages/Home/components/Header/Header.tsx
@@ -5,27 +5,25 @@ const Header = () => {
      return (
           <header className={styles.Header}>
                <div className={styles.info}>
-                    <div className={styles.infoBlock}>
-                         <div className={styles.infoLine}>
-                              <span className={styles.infoTitle}>{"Наш Email:"}</span>
-                              <span className={styles.infoText}>{"info@skelk.ru"}</span>
-                         </div>
-                         <div className={styles.infoLine}>
-                              <span className={styles.infoTitle}>{"Телефон:"}</span>
-                              <span className={styles.infoText}>{"+7 906-024-26-93"}</span>
-                         </div>
+                    <div className={styles.infoLine}>
+                         <span className={styles.infoTitle}>{"Наш Email:"}</span>
+                         <span className={styles.infoText}>{"info@skelk.ru"}</span>
                     </div>
-                    <div className={styles.infoBlock}>
-                         <div className={styles.infoLine}>
-                              <span className={styles.infoTitle}>{"Адрес:"}</span>
-                              <span className={styles.infoText}>{"г.Москва, ул Нагатинская 16"}</span>
-                         </div>
-                         <div className={styles.infoLine}>
-                              <span className={styles.infoTitle}>{"Время работы:"}</span>
-                              <span className={styles.infoText}>{"с 9:30 до 17:00"}</span>
-                         </div>
+                    <div className={styles.infoLine}>
+                         <span className={styles.infoTitle}>{"Телефон:"}</span>
+                         <span className={styles.infoText}>{"+7 906-024-26-93"}</span>
+                    </div>
+
+                    <div className={styles.infoLine}>
+                         <span className={styles.infoTitle}>{"Адрес:"}</span>
+                         <span className={styles.infoText}>{"г.Москва, ул Нагатинская 16"}</span>
+                    </div>
+                    <div className={styles.infoLine}>
+                         <span className={styles.infoTitle}>{"Время работы:"}</span>
+                         <span className={styles.infoText}>{"с 9:30 до 17:00"}</span>
                     </div>
                </div>
+
                <img className={styles.logo} src={logoImage} alt={"Логотип"} />
                <h1 className={styles.heading}>{"Скэлк - Склад электронных компонентов"}</h1>
           </header>
